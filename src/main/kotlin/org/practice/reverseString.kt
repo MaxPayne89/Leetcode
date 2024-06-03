26671b0eb9f46ed101affa66b27023aeddc36113
maximilianpergl <max.pergl@arcual.art>
2024-06-03 09:04:26 +0200

    feat:
    initial commit

---
+++ src/main/kotlin/org/practice/reverseString.kt
@@ -0,0 +1,17 @@
+package org.practice
+
+fun reverseString(s: CharArray): Unit {
+    var rightIndex = s.lastIndex
+    var leftIndex= 0
+
+    while (rightIndex > leftIndex) {
+        val r = s[rightIndex]
+        val l = s[leftIndex]
+
+        s[leftIndex] = r
+        s[rightIndex] = l
+
+        rightIndex -= 1
+        leftIndex += 1
+    }
+}
