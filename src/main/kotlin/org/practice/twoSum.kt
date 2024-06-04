cc908b42b95046db94f47b7262f7da0a2a9e31b1
maximilianpergl <max.pergl@arcual.art>
2024-06-04 20:13:22 +0200

    feat: TwoSum
    Add two sum algorithm

---
+++ src/main/kotlin/org/practice/twoSum.kt
@@ -0,0 +1,22 @@
+package org.practice
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = nums.withIndex().associateBy(keySelector = {target - it.value}, valueTransform = {it.index})
+    nums.forEachIndexed {index, value ->
+        val complementIndex = map[value]
+        complementIndex?.let {
+            if (index == complementIndex) {
+                Unit
+            } else {
+                return intArrayOf(index, complementIndex)
+            }
+        }
+    }
+    return intArrayOf()
+}
+
+fun main() {
+    val arr = intArrayOf(2,7,11,15)
+    val t = twoSum(arr, 9)
+    println(t)
+}
