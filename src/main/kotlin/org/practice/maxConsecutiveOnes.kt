a49a5ddb0f0fb5e56ce62b7e23ba3073d7d6a018
MaxPayne89 <maxpergl@gmail.com>
2024-06-21 07:26:04 +0200

    feat: consecutiveOnes
    add consecutive ones algorithm

---
+++ src/main/kotlin/org/practice/maxConsecutiveOnes.kt
@@ -0,0 +1,22 @@
+package org.practice
+
+fun findMaxConsecutiveOnes(nums: IntArray): Int {
+    var onesInARowMax = 0
+    var currOnesInARow = 0
+
+    for (i in nums.indices) {
+        if (nums[i] == 1) {
+            currOnesInARow++
+        } else {
+            onesInARowMax = if (currOnesInARow > onesInARowMax) currOnesInARow else onesInARowMax
+            currOnesInARow = 0
+        }
+    }
+    return maxOf(onesInARowMax, currOnesInARow)
+}
+
+fun main() {
+    val input = intArrayOf(1,1,0,1,1,1)
+    val a = findMaxConsecutiveOnes(input)
+    println(a)
+}
