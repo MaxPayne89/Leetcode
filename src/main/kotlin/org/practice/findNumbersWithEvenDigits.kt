7127b96a4931826d11628adacd92b9b4636a9361
MaxPayne89 <maxpergl@gmail.com>
2024-06-21 13:23:31 +0200

    feat: findEvenDigits
    add find even digits algorithm

---
+++ src/main/kotlin/org/practice/findNumbersWithEvenDigits.kt
@@ -0,0 +1,29 @@
+package org.practice
+
+fun findNumbers(nums: IntArray): Int {
+    var evenDigits = 0
+    nums.forEach { num ->
+        if (divideAndCount(num) % 2 == 0) {
+            evenDigits++
+        }
+    }
+    return evenDigits
+}
+
+fun main() {
+    val input = intArrayOf(12,345,2,6,7896)
+    val a = findNumbers(input)
+    println(a)
+
+//    println(divideAndCount(7896))
+}
+
+private fun divideAndCount(num: Int): Int {
+    var number = num
+    var counter = 0
+    while (number != 0) {
+        number /= 10
+        counter++
+    }
+    return counter
+}
