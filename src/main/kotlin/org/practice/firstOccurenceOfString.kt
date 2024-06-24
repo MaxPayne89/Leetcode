325a4c75ff3d201b41bbd217521d353d88e7044a
MaxPayne89 <maxpergl@gmail.com>
2024-06-24 15:11:33 +0200

    feat: First Ocurrence of String
    implement and optimize algorithm

---
+++ src/main/kotlin/org/practice/firstOccurenceOfString.kt
@@ -0,0 +1,29 @@
+package org.practice
+
+fun strStr(haystack: String, needle: String): Int {
+    return haystack.indexOf(needle)
+}
+
+fun strStrSlidingWindow(haystack: String, needle: String): Int {
+    val n = haystack.length
+    val m = needle.length
+
+    for (windowStart in 0 .. n-m) {
+        for (i in 0..needle.lastIndex) {
+            if (needle[i] != haystack[windowStart + i]) {
+                break
+            }
+            if (i == m - 1) {
+                return windowStart
+            }
+        }
+    }
+    return -1
+}
+
+fun main() {
+    val haystack = "sadbutsad"
+    val needle = "sad"
+    val a = strStr(haystack, needle)
+    println(a)
+}
