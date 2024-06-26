fe8fc71045b4a5117dd26a5a1f6edcfed3c13a1e
MaxPayne89 <maxpergl@gmail.com>
2024-06-26 07:24:47 +0200

    feat: H-Index
    add hIndex algorithm

---
+++ src/main/kotlin/org/practice/hIndex.kt
@@ -0,0 +1,19 @@
+package org.practice
+
+import java.util.*
+
+
+fun hIndex(citations: IntArray): Int {
+    Arrays.sort(citations)
+    var i = 0
+    while (i < citations.size && citations[citations.size - 1 - i] > i) {
+        i++
+    }
+    return i
+}
+
+fun main() {
+    val input = intArrayOf(3,0,6,1,5)
+    val a = hIndex(input)
+    println(a)
+}
