179e68f33675bd048473ff1260e8a3469a23578b
MaxPayne89 <maxpergl@gmail.com>
2024-06-28 20:02:52 +0200

    feat: Valid Mountain Array
    add algorithm

---
+++ src/main/kotlin/org/practice/validMountainArray.kt
@@ -0,0 +1,23 @@
+package org.practice
+
+fun validMountainArray(arr: IntArray): Boolean {
+    if (arr.size < 3) return false
+    var i = 0
+
+    while (i < arr.lastIndex && arr[i + 1] > arr[i]) {
+            i++
+    }
+    if (i == 0 || i == arr.lastIndex) return false
+
+    while (i < arr.lastIndex && arr[i + 1] < arr[i] ) {
+            i++
+    }
+
+    return i == arr.lastIndex
+}
+
+fun main() {
+    val input = intArrayOf(9,8,7,6,5,4,3,2,1,0)
+    val a = validMountainArray(input)
+    println(a)
+}
