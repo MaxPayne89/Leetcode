c216b611c8e5b42f0bd292d5442ca7f4f07949dd
MaxPayne89 <maxpergl@gmail.com>
2024-06-28 13:34:35 +0200

    feat: Binary Search
    implement int sqrt algorithm using binary search

---
+++ src/main/kotlin/org/practice/sqrt.kt
@@ -0,0 +1,32 @@
+package org.practice
+
+import kotlin.math.roundToInt
+
+fun mySqrt(x: Int): Int {
+    if (x == 0) return 0
+    if (x == 1 || x == 2 || x == 3) return 1
+    var left = 2
+    var right = x / 2
+    while (left <= right) {
+        val middle = (left + right) / 2
+
+        val square = middle.toLong() * middle
+        when {
+            (square == x.toLong()) -> {
+                return middle
+            }
+            square > x -> {
+                right = middle - 1
+            }
+            else -> {
+                left = middle + 1
+            }
+        }
+    }
+    return (left + right) / 2
+}
+
+fun main() {
+    val a = mySqrt(2147395599)
+    println(a)
+}
