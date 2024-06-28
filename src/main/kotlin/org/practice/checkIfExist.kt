93daf28b360493fe354ee1e62a6240e7e1f37161
MaxPayne89 <maxpergl@gmail.com>
2024-06-28 14:40:02 +0200

    feat: Check if Double Exists
    implement algorithm

---
+++ src/main/kotlin/org/practice/checkIfExist.kt
@@ -0,0 +1,21 @@
+package org.practice
+
+fun checkIfExist(arr: IntArray): Boolean {
+    val map = mutableMapOf<Int,Int>()
+    arr.forEachIndexed { index, i ->
+        map[i] = index
+    }
+    for (i in arr.indices) {
+        if (map.contains(2 * arr[i]) && map[2 * arr[i]] != i) {
+            return true
+        }
+    }
+
+    return false
+}
+
+fun main() {
+    val input = intArrayOf(4,-7,11,4,18)
+    val a = checkIfExist(input)
+    println(a)
+}
