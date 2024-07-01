dabe00824b05b7c96e4ef072b481d36cf855c164
MaxPayne89 <maxpergl@gmail.com>
2024-07-01 10:09:38 +0200

    feat: Guess Higher or Lower
    implement algorithm using binary search

---
+++ src/main/kotlin/org/practice/GuessHigherOrLower.kt
@@ -0,0 +1,41 @@
+package org.practice
+
+class Solution {
+    fun guessNumber(n:Int):Int {
+        var low = 1L
+        var high = n.toLong()
+
+        while (low <= high) {
+            when (guess(((low + high) / 2).toInt())) {
+                1 -> {
+                    low = ((low + high) / 2) + 1
+                }
+                -1 -> {
+                    high = ((low + high) / 2) - 1
+                }
+                0 -> {
+                    return ((low + high) / 2).toInt()
+                }
+            }
+        }
+        return 0
+    }
+}
+
+fun guess(n: Int): Int {
+    val pick = 1702766719
+    return when {
+        n < pick -> 1
+        n > pick -> -1
+        else -> 0
+    }
+}
+
+fun main() {
+    val s = Solution()
+    val a = s.guessNumber(2126753390)
+    println(a)
+}
+
+//2126753390
+//1702766719
