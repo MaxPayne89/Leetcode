2fc0dc95374163065794af37f90754ffdbdf25de
maxarcual <max.pergl@arcual.art>
2024-08-08 09:45:31 +0200

    feat(Sliding Window): Longest Sub Array of 1s
    optimize algorithm

--- src/main/kotlin/org/practice/slidingWindow/longestSubArrOfOnes.kt
+++ src/main/kotlin/org/practice/slidingWindow/longestSubArrOfOnes.kt
@@ -1,31 +1,43 @@
 package org.practice.slidingWindow
 
+import org.practice.binarySearch.findPeakElement
 import kotlin.math.max
 
 fun longestSubarray(nums: IntArray): Int {
     var lefty = 0
-    var righty = 0
+//    var righty = 0
     var maxSeq = 0
     var numZeroes = 0
 
-    while (righty < nums.size) {
-        if (nums[righty] == 0) {
-            numZeroes++
-        }
-
+    for (i in nums.indices) {
+        numZeroes += if (nums[i] == 0) 1 else 0
         while (numZeroes > 1) {
-            if (nums[lefty] == 0) {
-                numZeroes--
-            }
+            numZeroes -= if (nums[lefty] == 0) 1 else 0
             lefty++
         }
 
-        maxSeq = max(maxSeq, righty - lefty + 1)
-
-        righty++
+        maxSeq = max(maxSeq, i - lefty)
     }
 
-    return if (numZeroes == 1 || maxSeq == nums.size) maxSeq - 1 else maxSeq
+//    while (righty < nums.size) {
+//        if (nums[righty] == 0) {
+//            numZeroes++
+//        }
+//
+//        while (numZeroes > 1) {
+//            if (nums[lefty] == 0) {
+//                numZeroes--
+//            }
+//            lefty++
+//        }
+//
+//        maxSeq = max(maxSeq, righty - lefty + 1)
+//
+//        righty++
+//    }
+
+//    return if (numZeroes == 1 || maxSeq == nums.size) maxSeq - 1 else maxSeq
+    return maxSeq
 }
 
 fun main() {
