f7b1de4deebe4419a2b4a4a0e69e4c348b683956
maxarcual <max.pergl@arcual.art>
2024-08-10 13:16:39 +0200

    feat(Stack): Remove Stars from a String
    optimize the stack approach a bit

--- src/main/kotlin/org/practice/string/removeStarsFromAString.kt
+++ src/main/kotlin/org/practice/string/removeStarsFromAString.kt
@@ -3,27 +3,32 @@ package org.practice.string
 import java.util.Stack
 
 fun removeStars(s: String): String {
-//    val stack = Stack<Char>()
-//    for (i in s.indices) {
+    val stack = Stack<Char>()
+    for (i in s.indices) {
+        if (s[i] != '*') {
+            stack.push(s[i])
+        } else {
+            stack.pop()
+        }
 //        stack.push(s[i])
 //        if (stack.peek() == '*') {
 //            stack.pop()
 //            stack.pop()
 //        }
-//    }
-//    return stack.elements().toList().joinToString(separator = "")
-
-    val result = StringBuilder()
-    for (char in s) {
-        if (char == '*') {
-            if (result.isNotEmpty()) {
-                result.deleteCharAt(result.length - 1)
-            }
-        } else {
-            result.append(char)
-        }
     }
-    return result.toString()
+    return stack.elements().toList().joinToString(separator = "")
+
+//    val result = StringBuilder()
+//    for (char in s) {
+//        if (char == '*') {
+//            if (result.isNotEmpty()) {
+//                result.deleteCharAt(result.length - 1)
+//            }
+//        } else {
+//            result.append(char)
+//        }
+//    }
+//    return result.toString()
 }
 
 fun main() {
